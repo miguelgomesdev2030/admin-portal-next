3c13f7588253e1f92eb277dd958b29ab05286b3a
miguelgomesdev2030 <miguelgomesdev2030@gmail.com>
2025-03-04 18:41:15 +0100

    update api baseURL with AWS

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL:
+    "http://bookingsystem-env-1.eba-ipbed9av.eu-west-3.elasticbeanstalk.com",
   withCredentials: true,
 });
