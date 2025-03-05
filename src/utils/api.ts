009f4c7fca9b06a80ba6bb596e4b3edd8bc1c076
miguelgomesdev2030 <miguelgomesdev2030@gmail.com>
2025-03-05 10:04:59 +0100

    fix: update backend URL to production server

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "https://vq5gyh-8080.csb.app";
 export const api = axios.create({
   baseURL: BACKEND_URL,
   withCredentials: true,
